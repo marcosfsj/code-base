3da9f0362223cf53df10a11650ee0e909bb7287c
Fernandes <marcfernandes@teksystems.com>
2018-10-17 14:12:59 -0400

    Adding new Shortener pipes

---
+++ src/app/pipes/shortener.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+export class ShortenerPipe implements PipeTransform {
+    
+    transform(value: any, limit: number) {
+        if(value.length > limit) {
+            return value.substr(0, limit) + ' ...';
+        }
+        return value;
+    }
+
+}
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Foo } from '../model';
 
 @Injectable({
   providedIn: 'root'
